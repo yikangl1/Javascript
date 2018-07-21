ec08ebc31e0c89456f062e1609c767dbc6e3ec48
yikangl1 <yikangl1@uci.edu>
2018-07-21 08:09:45 -0700

    Add files via upload

---
+++ Github Search/github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '23055147eed957226427';
+    this.client_secret = 'e1bbf6237307f1ac1ece19f37abdb439f3c36779';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
